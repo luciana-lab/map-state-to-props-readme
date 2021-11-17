72411a8a896482568af9443e52de67226d6417fb
Luciana Sugeta <lulusugeta@gmail.com>
2021-11-17 17:07:56 -0500

    commit tree 289285fbbbe198430758c79c86fea724511c2184

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+
+// Import connect() function from React Redux
+// Connect(), allows us to specify which data we are listening to (through mapStateToProps)
+// and which component we are providing the data to (App).
+import { connect } from 'react-redux';
 import './App.css';
 
 class App extends Component {
 	handleOnClick = () => {
-		this.props.store.dispatch({
+		// this.props.store.dispatch({
+		// reflect the fact that the store is no longer being passed directly to App from index.js
+		// dispatch is automatically provided by connect
+		this.props.dispatch({
 			type: 'INCREASE_COUNT',
 		});
 	}
@@ -12,10 +20,50 @@ class App extends Component {
 		return (
 			<div className="App">
 				<button onClick={this.handleOnClick}>Click</button>
-				<p>{this.props.store.getState().clicks}</p>
+				{/* <p>{this.props.store.getState().clicks}</p> */}
+				{/* reflect the fact that the store is no longer being passed directly to App from index.js */}
+				<p>{this.props.clicks}</p>
 			</div>
 		);
 	}
 }
 
-export default App;
+// create new method, mapStateToProps
+
+// 2 goals:
+// 1. Only re-render the App component when specific changes to the state occur
+// 2. Only provide the needed slice of the state to the App component
+
+// What we'll need to achieve these goals:
+// 1. A function that listens to every change in the store  (connect function)
+// 2. Accesses the changes relevant to a particular component (mapStateToProps and the render using this.props.clicks)
+// 3. Provide to that component
+
+// Inside here we specify exactly which slice of the state we want to provide to the component (state.clicks)
+// Allow our component to have access to through a prop called clicks
+// We're able to render the number of clicks in the render using this.props.clicks
+const mapStateToProps = state => {
+	return { clicks: state.clicks }
+}
+
+// modify the export statement to use connect to wire everything together
+// export default App;
+
+// connect function is synced up to the store, listening to each change in the state that occurs
+// When a change occurs, it calls the mapStateToProps function
+
+// connect(mapStateToProps)(App) specify that we are connecthing this state to the App
+// connect() return a new component, similar to App but is connected up to receive the correct data
+// This new compoennt is the component we wish to export
+export default connect(mapStateToProps)(App);
+
+// Connection a component to the store means that it'll be able to get data from the store's internal state
+// It'll be re-remder and get new data when that state changes
+
+
+// const mapStateToProps = state => {
+	// return { clicks: state.clicks }
+// }
+// connect(mapStateToProps)(App)
+// These 4 lines above are saying connect the data in mapStateToProps() (the clicks portion of the state) to the App component. 
+// And the App component can access that state with this.props.clicks
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+
+// Provider ensures that our entire React application can potentially access data from the store. 
+// Provider component from React Redux does 2 things:
+// 1. It'll make the store available to nested components once they have been configured using connect() - a 2nd method provided by React Redux library
+// 2. It alerts our Redux app when there has been a change in state, which will then re-render our React app
+import { Provider } from 'react-redux';
 import counterReducer from './reducers/counterReducer.js';
 import App from './App';
 import './index.css';
@@ -8,6 +14,15 @@ import './index.css';
 const store = createStore(
     counterReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-); 
+);
 
-ReactDOM.render(<App store={store}/>, document.getElementById('root'));
+// Wrap the Provider component around the App component
+// Instead of passing the store instance directly into the App component, pass it in to Provider as a prop, which will make it available to other components
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
+
+// Using the <Provider> component provided by the React Redux library, we gave our components the ability to be connected to the store
